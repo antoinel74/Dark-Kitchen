1f9d0795384e9ed6eb38a7c40389b82362b0005b
Carole Gérard <gerard.carole78@gmail.com>
2023-09-26 16:42:27 +0200

    _dark-mode.scss

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,21 +1,30 @@
-const darkMode = document.querySelector('.darkmode');
+let darkMode = document.querySelector('#darkmode-toggle');
+
+
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    darkMode.checked = false;
+    console.log(darkMode.checked);
+});
 
 
 darkMode.addEventListener('click', () => {
+    
+    console.log(darkMode.checked);
 
     const body = document.body;
     
-    if(body.classList.contains('dark')){
+    if(!darkMode.checked){
 
         body.classList.add('light')
         body.classList.remove('dark')
-        darkMode.textContent = "🌞"
+        // darkMode.textContent = "🌞"
         
-    } else if(body.classList.contains('light')){
+    } else if(darkMode.checked){
 
         body.classList.add('dark')
         body.classList.remove('light')
-        darkMode.textContent = "🌙"
+        // darkMode.textContent = "🌙"
 
     }
 })
